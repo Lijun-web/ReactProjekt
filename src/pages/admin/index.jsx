9e4776a229d2f7ce816bd1c2a19723484e6f43eb
lijun <wanglijun206@gmail.com>
2021-05-25 20:29:18 +0200

    init

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+/*
+managing-routing-component
+ */
+export default class Admin extends Component {
+    render() {
+        return (
+            <div>Admin....</div>
+        )
+    }
+}
---
+++ src/pages/login/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import "./login.less";
+
+/*
+Login-Routing-Component
+ */
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="login" >
+                <div className="login-header"></div>
+                <div className="login-content"></div>
+            </div>
+        )
+    }
+}
